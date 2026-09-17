0a5f6f288ee6ba8017042957d284f85ee236732f
kamala228 <kamala228@DESKTOP-GAMF57I>
2026-09-17 09:23:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp57/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] first = { 1, 2, 3 };
+int[] second = { 4, 5, 6, 7 };
+
+int[] result = MergeArrays(first, second);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(result[i] + " ");
+}
+
+Console.ReadKey();
+
+int[] MergeArrays(int[] a, int[] b)
+{
+    int[] res = new int[a.Length + b.Length];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        res[i] = a[i];
+    }
+
+    for (int i = 0; i < b.Length; i++)
+    {
+        res[a.Length + i] = b[i];
+    }
+
+    return res;
+}
